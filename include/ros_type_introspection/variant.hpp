6238ad69e49ab68d6661b17a8dd99f261954314a
Davide Faconti <davide.faconti@gmail.com>
2017-06-23 10:50:32 +0200

    bug fix. types where missing in conversion

--- include/ros_type_introspection/variant.hpp
+++ include/ros_type_introspection/variant.hpp
@@ -70,7 +70,7 @@ inline T VarNumber::extract( ) const
 
     if( getTypeID() != RosIntrospection::getType<T>() )
     {
-        throw TypeException("ops");
+        throw TypeException("VarNumber::extract -> wrong type");
     }
     return * reinterpret_cast<const T*>( _raw_data );
 }
@@ -92,12 +92,17 @@ inline DST VarNumber::convert() const
     //----------
     switch( _raw_data[8] )
     {
+    case CHAR:
     case INT8:   convert_impl<int8_t,  DST>(*reinterpret_cast<const int8_t*>( _raw_data), target  ); break;
+
     case INT16:  convert_impl<int16_t, DST>(*reinterpret_cast<const int16_t*>( _raw_data), target  ); break;
     case INT32:  convert_impl<int32_t, DST>(*reinterpret_cast<const int32_t*>( _raw_data), target  ); break;
     case INT64:  convert_impl<int64_t, DST>(*reinterpret_cast<const int64_t*>( _raw_data), target  ); break;
 
+    case BOOL:
+    case BYTE:
     case UINT8:   convert_impl<uint8_t,  DST>(*reinterpret_cast<const uint8_t*>( _raw_data), target  ); break;
+
     case UINT16:  convert_impl<uint16_t, DST>(*reinterpret_cast<const uint16_t*>( _raw_data), target  ); break;
     case UINT32:  convert_impl<uint32_t, DST>(*reinterpret_cast<const uint32_t*>( _raw_data), target  ); break;
     case UINT64:  convert_impl<uint64_t, DST>(*reinterpret_cast<const uint64_t*>( _raw_data), target  ); break;
@@ -105,7 +110,7 @@ inline DST VarNumber::convert() const
     case FLOAT32:  convert_impl<float, DST>(*reinterpret_cast<const float*>( _raw_data), target  ); break;
     case FLOAT64:  convert_impl<double, DST>(*reinterpret_cast<const double*>( _raw_data), target  ); break;
 
-    default:  throw TypeException("ops"); break;
+    default:  throw TypeException("VarNumber::extract -> cannot convert type" + std::to_string(_raw_data[8])); break;
 
     }
     return  target;
