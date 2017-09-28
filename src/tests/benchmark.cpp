fc69f747517381000426ded7be873f2311da86e9
Davide Faconti <davide.faconti@gmail.com>
2017-09-28 15:28:33 +0200

    moved to google benchmarking

--- src/tests/benchmark.cpp
+++ src/tests/benchmark.cpp
@@ -9,16 +9,17 @@
 #include <iostream>
 #include <chrono>
 #include <ros_type_introspection/renamer.hpp>
+#include <benchmark/benchmark.h>
 
 using namespace ros::message_traits;
 using namespace RosIntrospection;
 
 
-std::vector<SubstitutionRule> Rules()
+static std::vector<SubstitutionRule> Rules()
 {
-    std::vector<SubstitutionRule> rules;
+  std::vector<SubstitutionRule> rules;
 
-/*
+  /*
     rules.push_back( SubstitutionRule( "transforms.#.transform",
                                        "transforms.#.header.frame_id",
                                        "transforms.#" ));
@@ -27,67 +28,57 @@ std::vector<SubstitutionRule> Rules()
                                        "transforms.#.header.frame_id",
                                        "transforms.#.header" ));*/
 
-    rules.push_back( SubstitutionRule( "position.#", "name.#", "@.position" ));
-    rules.push_back( SubstitutionRule( "velocity.#", "name.#", "@.velocity" ));
-    rules.push_back( SubstitutionRule( "effort.#",   "name.#", "@.effort"   ));
-    return rules;
+  rules.push_back( SubstitutionRule( "position.#", "name.#", "@.position" ));
+  rules.push_back( SubstitutionRule( "velocity.#", "name.#", "@.velocity" ));
+  rules.push_back( SubstitutionRule( "effort.#",   "name.#", "@.effort"   ));
+  return rules;
 }
 
 
-int main( int argc, char** argv)
-{
+static void BM_Joint(benchmark::State& state) {
 
-    ROSTypeList type_map =  buildROSTypeMapFromDefinition(
-                DataType<sensor_msgs::JointState >::value(),
-                Definition<sensor_msgs::JointState>::value() );
+  ROSTypeList type_map =  buildROSTypeMapFromDefinition(
+        DataType<sensor_msgs::JointState >::value(),
+        Definition<sensor_msgs::JointState>::value() );
 
-    std::cout << "------------------------------"  << std::endl;
-    std::cout << type_map << std::endl;
+  sensor_msgs::JointState js_msg;
 
-    sensor_msgs::JointState js_msg;
+  js_msg.name.resize(6);
+  js_msg.position.resize(6);
+  js_msg.velocity.resize(6);
+  js_msg.effort.resize(6);
 
-    js_msg.name.resize(6);
-    js_msg.position.resize(6);
-    js_msg.velocity.resize(6);
-    js_msg.effort.resize(6);
+  const char* suffix[6] = { "_A", "_B", "_C", "_D" , "_E", "_F"};
 
-    const char* suffix[6] = { "_A", "_B", "_C", "_D" , "_E", "_F"};
+  for (int i=0; i< js_msg.name.size() ; i++)
+  {
+    js_msg.header.seq = 100+i;
+    js_msg.header.stamp.sec = 1234;
+    js_msg.header.frame_id = std::string("frame").append(suffix[i]);
 
-    for (int i=0; i< js_msg.name.size() ; i++)
-    {
-        js_msg.header.seq = 100+i;
-        js_msg.header.stamp.sec = 1234;
-        js_msg.header.frame_id = std::string("frame").append(suffix[i]);
+    js_msg.name[i] = std::string("child").append(suffix[i]);
+    js_msg.position[i]  = 10 +i;
+    js_msg.velocity[i]  = 20 +i;
+    js_msg.effort[i]    = 30 +i;
+  }
 
-        js_msg.name[i] = std::string("child").append(suffix[i]);
-        js_msg.position[i]  = 10 +i;
-        js_msg.velocity[i]  = 20 +i;
-        js_msg.effort[i]    = 30 +i;
-    }
+  std::vector<uint8_t> buffer(64*1024);
+  ros::serialization::OStream stream(buffer.data(), buffer.size());
+  ros::serialization::Serializer<sensor_msgs::JointState>::write(stream, js_msg);
+  ROSType main_type (DataType<sensor_msgs::JointState>::value());
 
-    std::vector<uint8_t> buffer(64*1024);
+  auto rules = Rules();
 
-    auto start = std::chrono::high_resolution_clock::now();
+  ROSTypeFlat flat_container;
+  RenamedValues renamed;
 
-    ros::serialization::OStream stream(buffer.data(), buffer.size());
-    ros::serialization::Serializer<sensor_msgs::JointState>::write(stream, js_msg);
-    ROSType main_type (DataType<sensor_msgs::JointState>::value());
-
-    auto rules = Rules();
-
-    ROSTypeFlat flat_container;
-    RenamedValues renamed;
-    for (int i=0; i<100*1000;i++)
-    {
-        buildRosFlatType(type_map, main_type, "joint_state", buffer.data(), &flat_container, 100);
-        applyNameTransform( rules , flat_container, renamed );
-    }
-
-    auto end = std::chrono::high_resolution_clock::now();
-    auto elapsed = end - start;
-
-    std::cout << "time elapsed: " << std::chrono::duration_cast< std::chrono::milliseconds>( elapsed ).count()  <<   std::endl;
-
-    return 0;
+  while (state.KeepRunning())
+  {
+    buildRosFlatType(type_map, main_type, "joint_state", buffer.data(), &flat_container, 100);
+    applyNameTransform( rules , flat_container, renamed );
+  }
 }
 
+BENCHMARK(BM_Joint);
+
+BENCHMARK_MAIN();
